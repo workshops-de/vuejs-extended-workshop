571f747168452f04191163761a01ec2774e2e7ea
Antony Konstantinidis <ak@digitalessence.de>
2020-06-03 14:27:08 +0200

    Implement a 'dummy' directive

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-param-reassign': [2, {
+      'props': false,
+    }],
   },
 };
