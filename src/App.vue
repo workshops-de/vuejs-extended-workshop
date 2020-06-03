6dae0d0b048bfaccc3f2d1f5d45a314363a06ae3
Antony Konstantinidis <ak@digitalessence.de>
2020-06-03 14:27:08 +0200

    Implement a 'dummy' directive

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="app">
+    <h2>custom dummy directive</h2>
+    <p v-dummy="10">Should output 10: </p>
+    <p v-dummy="'2,5'">Is between 2 and 5: </p>
+    <p v-dummy="dataVal"></p>
+    <p v-dummy:image="100">See the following image --> </p>
+    <p v-dummy:image="'400,100x100,400'"></p>
+    <p v-dummy:image="dataVal"></p>
+    <h2>custom filter</h2>
     <div>
       Without filter:
       {{ msg }}
@@ -8,6 +16,7 @@
       With filter:
       {{ msg | truncate(11, '...') }}
     </div>
+    <h2>custom format directive</h2>
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
@@ -24,6 +33,7 @@ export default Vue.extend({
   data() {
     return {
       msg: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      dataVal: 255,
     };
   },
 });
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,34 @@
 import Vue from 'vue';
+import Dummy from 'dummyjs';
 import App from './App.vue';
 
 Vue.config.productionTip = false;
 
 Vue.filter('truncate', (text: string, length = 10, suffix = '') => text.substring(0, length) + suffix);
+
+Vue.directive('dummy', (el, binding) => {
+  if (!el) {
+    return;
+  }
+
+  const allowedTypes = {
+    text: /^\d+,\d+$/,
+    image: /^\d+,\d+x\d+,\d+$/,
+  };
+  const { value, arg = 'text' } = binding;
+
+  if (!Object.keys(allowedTypes).includes(arg)) {
+    return;
+  }
+
+  if (typeof value === 'number' || (typeof value === 'string' && value.match(allowedTypes[arg as 'text' | 'image']))) {
+    if (arg === 'text') {
+      el.innerHTML += Dummy.text(value);
+    } else {
+      el.innerHTML += `<img alt="Dummy image ${value}" src="${Dummy.image(binding.value)}" />`;
+    }
+  }
+});
 
 new Vue({
   render: (h) => h(App),
