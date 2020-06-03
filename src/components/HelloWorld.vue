862f1b6794cac2fcd95cf686ddbc7d4740bec857
Antony Konstantinidis <ak@digitalessence.de>
2020-06-03 10:54:04 +0200

    Create a 'format' custom directive

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,13 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
+    </p>
+    <p>For a <span v-format.underline>guide</span> and <span v-format.bold>recipes</span> on how to
+      <span v-format.highlight>configure / customize</span> this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3><span v-format.underline.bold.highlight>Installed CLI Plugins</span></h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
@@ -38,6 +40,21 @@ export default Vue.extend({
   name: 'HelloWorld',
   props: {
     msg: String,
+  },
+  directives: {
+    /* eslint-disable no-param-reassign */
+    format(el, binding) {
+      const { modifiers } = binding;
+      if (modifiers.underline) {
+        el.style.textDecoration = 'underline';
+      }
+      if (modifiers.bold) {
+        el.style.fontWeight = 'bold';
+      }
+      if (modifiers.highlight) {
+        el.style.background = '#ffff00';
+      }
+    },
   },
 });
 </script>
